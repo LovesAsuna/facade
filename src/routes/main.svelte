2a26fa59cfd64c1af2010c276d0e3fdedd038a14
LovesAsuna <qq625924077@gmail.com>
2025-05-01 21:48:35 +0800

    :recycle: refactor(framework): refactor with svelte

---
+++ src/routes/main.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Intro from './intro.svelte'
+    import About from './about.svelte'
+    import Find from './find.svelte'
+    import Gate from './gate.svelte'
+    import Footer from './footer.svelte'
+</script>
+
+<div class="main">
+    <Intro />
+    <About />
+    <Find />
+    <Gate />
+    <Footer />
+</div>
