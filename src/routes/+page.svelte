853ea3d17cf710e36a837382b286bb7f957284d9
LovesAsuna <qq625924077@gmail.com>
2025-05-01 21:48:35 +0800

    :recycle: refactor(framework): refactor with svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import Background from './background.svelte'
+    import Navigation from './navigation.svelte'
+    import Main from './main.svelte'
+</script>
+
+<div id="app">
+    <Navigation />
+    <Background />
+    <Main />
+</div>
+
+<style>
+    :global(*) {
+        padding: 0;
+        margin: 0;
+        transition-timing-function: cubic-bezier(.19, 1, .22, 1)
+    }
+
+    :global(body, html) {
+        background-color: #212121;
+    }
+
+    :global(body, html) {
+        font-family: "Microsoft YaHei",serif;
+        -webkit-font-smoothing: antialiased;
+        color: #fff;
+        line-height: 1.5;
+        font-size: 14px;
+        overflow-x: hidden
+    }
+</style>
---
+++ src/routes/footer.svelte
@@ -0,0 +1,49 @@
+<div class="footer ch">
+    <div class="container">
+        <h3>冰星@LovesAsuna</h3>
+        <p>如果在靠近一点，是否还会有感觉</p>
+        <p class="c">
+            <a href="https://blog.hyosakura.com/">Say hello to me @ hyosakura.com</a>
+        </p>
+    </div>
+</div>
+<style>
+    .container {
+        position: relative;
+        width: auto;
+        margin: 0 auto;
+        height: 100%;
+        padding: 20px 0
+    }
+
+    .footer {
+        text-align: center
+    }
+
+    .footer a {
+        color: inherit;
+        text-decoration: none
+    }
+
+    .footer a:hover {
+        text-decoration: underline
+    }
+
+    .footer h3 {
+        font-weight: 400;
+        font-size: 20px;
+        letter-spacing: .8em;
+        margin: 6px 0
+    }
+
+    .footer p {
+        font-size: 12px;
+        letter-spacing: 1em;
+        opacity: .3
+    }
+
+    .footer p.c {
+        margin-top: 20px;
+        letter-spacing: .1em
+    }
+</style>
